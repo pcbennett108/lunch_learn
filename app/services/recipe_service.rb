0ac89ae967108edb6bf16d6f3a8c079bba6b512b
pcbennett108 <pcbennett42@gmail.com>
2023-11-12 21:06:22 -0700

    Add recipe service, route and controller

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,22 @@
+class RecipeService
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.edamam.com') do |faraday|
+      faraday.params['type'] = 'public'
+      faraday.params['app_id'] = '1c8b7d28'
+      faraday.params['app_key'] = Rails.application.credentials.edamam[:key]
+    end
+  end
+
+  def recipes_by_country(country)
+    get_url("/api/recipes/v2?q=#{country}")
+    
+  end
+
+
+end
